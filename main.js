740a7b398208baf7a89149960961d0e7161670d4
tosbas <rootadm01@gmail.com>
2021-07-29 12:50:33 +0200

    amélioration

--- main.js
+++ main.js
@@ -1,16 +1,51 @@
 "Use strict";
-let touches = [...document.querySelectorAll(".touches")];
-let ecran = document.getElementById("ecran");
-let saisis = document.getElementById("saisis");
-let reset = document.getElementById("reset");
-let egale = document.getElementById("egale");
-let emptyMsg = { text: "Rien à calculer", value: undefined };
+const touches = [...document.querySelectorAll(".touches")];
+const ecran = document.getElementById("ecran");
+const saisis = document.getElementById("saisis");
+const reset = document.getElementById("reset");
+const egale = document.getElementById("egale");
+const emptyMsg = { text: "Rien à calculer", value: undefined };
+
+// event touches au clavier 
+
+document.addEventListener("keydown", (e) => {
+
+  let reg = /[0-9,+,\-,\/,*]/gm.test(e.key)
+
+  if(saisis.innerText.length == 14)
+  {
+    ecran.style.overflowY = "scroll"
+  }
+
+  if(e.key == "Backspace")
+  {
+    ecran.removeAttribute("style");
+    saisis.innerText = "";
+  }
+  else if(e.key == "Enter")
+  {
+    if(saisis.innerText != "")
+    {
+      saisis.innerText = arrondir(eval(saisis.innerText));
+    }
+  }
+  else if(reg == true){
+    saisis.innerText += e.key
+  }
+})
+
+// Pour chaque touches event au click
 
 touches.forEach((element) => {
   element.addEventListener("click", function () {
     element.classList.add("anim");
 
     saisis.innerText += element.innerText;
+
+    if(saisis.innerText.length == 14)
+    {
+      ecran.style.overflowY = "scroll"
+    }
 
     setTimeout(function () {
       element.classList.remove("anim");
@@ -20,15 +55,22 @@ touches.forEach((element) => {
 
 // calcule la somme saisis
 
-egale.addEventListener("click", function () {
-  saisis.innerText = arrondir(eval(saisis.innerText));
+egale.addEventListener("click", () => {
+  if(saisis.innerText != "")
+  {
+    saisis.innerText = arrondir(eval(saisis.innerText));
+  }
+  
 });
 
 // reset saisis
 
-reset.addEventListener("click", function () {
-  saisis.innerText = "";
+reset.addEventListener("click", () => {
+    ecran.removeAttribute("style");
+    saisis.innerText = "";
 });
+
+// function arrondir le résultat
 
 function arrondir(el) {
   let reg = /[.][1-9][0]/g.test(el);
